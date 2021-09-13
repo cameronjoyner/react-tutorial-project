bcfcaaeb6b51db01c528b88d4ddda153a1f5cbe5
cameronjoyner <cameronjoyner@gmail.com>
2021-09-13 12:45:30 -0400

    started with TS and react to build a new project

---
+++ advancedHooks/src/pages/index.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import styled from "styled-components"
+
+const IndexPage = () => (
+    <Wrapper>
+      <Logo src="/images/logos/react-logo.svg" alt="logo"/>
+    </Wrapper>
+)
+
+export default IndexPage
+
+const Wrapper = styled.div`
+  height: 1247px;
+  background: linear-gradient(200.44deg, #4316db 13.57%, #9076e7 98.38%);
+`
+
+const Logo = styled.img`
+  width: 60px;
+  height: 60px;
+`
